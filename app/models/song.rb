7983a5486c5bb44eb186c879f6abfc18f1e0adf0
Veronica Lopez <nicaa0695@gmail.com>
2019-09-28 23:59:32 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,8 +4,11 @@ class Song < ActiveRecord::Base
   def artist_name
     if artist
       artist.name
-    end
+  end
     
-  
+  def artist_name=(name)
+    my_artist = Artist.find_or_create_by(name: name)
+    self.artist = my_artist
+  end	  
 
 end
